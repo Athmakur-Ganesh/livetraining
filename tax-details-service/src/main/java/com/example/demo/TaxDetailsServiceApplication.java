1c7fb2e1d4cb9137d70f0796356aadf004c95600
Srivatsan <'vatsank@gmail.com'>
2020-07-20 17:04:47 +0530

    Update

--- tax-details-service/src/main/java/com/example/demo/TaxDetailsServiceApplication.java
+++ tax-details-service/src/main/java/com/example/demo/TaxDetailsServiceApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.TaxDetail;
+import com.example.demo.repos.TaxDetailsRepository;
 
 @SpringBootApplication
 public class TaxDetailsServiceApplication {
@@ -10,4 +16,19 @@ public class TaxDetailsServiceApplication {
 		SpringApplication.run(TaxDetailsServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+		
+			@Autowired
+			TaxDetailsRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+				
+				repo.save(new TaxDetail("aa11", "employed", "vignesh", "chennai-1"));
+			}
+		};
+	}
 }
