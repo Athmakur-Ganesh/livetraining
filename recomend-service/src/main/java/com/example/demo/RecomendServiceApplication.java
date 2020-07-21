7ab2d5de61e49a9fc1faabfb4766819276822ab7
Srivatsan <'vatsank@gmail.com'>
2020-07-21 14:34:56 +0530

    AOP Example Added

---
+++ recomend-service/src/main/java/com/example/demo/RecomendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendServiceApplication.class, args);
+	}
+
+}
---
+++ recomend-service/src/main/java/com/example/demo/entity/Recommendation.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name="synrecomend")
+@AllArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Recommendation {
+
+	
+	@Id
+	private int id;
+	private String productName;
+	private String description;
+}
