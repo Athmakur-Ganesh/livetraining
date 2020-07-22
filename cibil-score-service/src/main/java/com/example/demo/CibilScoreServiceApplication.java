87222e547c827cbd36f4cfce0dd0efcf27793954
Srivatsan <'vatsank@gmail.com'>
2020-07-22 12:21:00 +0530

    Update

--- cibil-score-service/src/main/java/com/example/demo/CibilScoreServiceApplication.java
+++ cibil-score-service/src/main/java/com/example/demo/CibilScoreServiceApplication.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.CibilScore;
+import com.example.demo.repos.CibilScoreRepository;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +18,24 @@ public class CibilScoreServiceApplication {
 		SpringApplication.run(CibilScoreServiceApplication.class, args);
 	}
 
+	
+	@Bean
+	public CommandLineRunner runner() {
+		
+		return new CommandLineRunner() {
+			
+			@Autowired
+			CibilScoreRepository repo;
+			@Override
+			public void run(String... args) throws Exception {
+				// TODO Auto-generated method stub
+				
+				repo.save(new CibilScore(101,"aa","Ramesh",560));
+				repo.save(new CibilScore(102,"bb","Rakesh",660));
+				repo.save(new CibilScore(103,"cc","Rajesh",760));
+
+			}
+		};
+		
+	}
 }
