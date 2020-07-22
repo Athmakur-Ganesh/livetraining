527a4a0e1df3fd3ce8420db6f7a3a36e1e7862e8
Srivatsan <'vatsank@gmail.com'>
2020-07-22 12:13:45 +0530

    Update

---
+++ cibil-score-service/src/main/java/com/example/demo/controllers/CibilScoreController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.CibilScore;
+import com.example.demo.repos.CibilScoreRepository;
+
+import lombok.Setter;
+
+@RestController
+@CrossOrigin(origins = "*")
+@Setter
+public class CibilScoreController {
+
+	@Autowired
+	private CibilScoreRepository service;
+	
+	
+	@GetMapping(path = "/api/v1/scores")
+	public List<CibilScore> getAll(){
+		
+		return this.service.findAll();
+	}
+	
+}
