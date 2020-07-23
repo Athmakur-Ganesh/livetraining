d9a8e999fc80d64b4d9462fdbd54b27627919d8d
Srivatsan <'vatsank@gmail.com'>
2020-07-23 11:08:29 +0530

    Update

--- cibil-score-service/src/main/java/com/example/demo/controllers/CibilScoreController.java
+++ cibil-score-service/src/main/java/com/example/demo/controllers/CibilScoreController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,9 @@ public class CibilScoreController {
 
 	@Autowired
 	private CibilScoreRepository service;
-	
+
+	@Value("${server.port}")
+	private String port;
 	
 	@GetMapping(path = "/api/v1/scores",produces = "application/json")
 	public List<CibilScore> getAll(){
@@ -28,5 +31,15 @@ public class CibilScoreController {
 		return this.service.findAll();
 	}
 	
+
+	@GetMapping(path = "/api/v1/scores/{id}",produces = "application/json")
+	public CibilScore findById(@PathVariable("id") int id){
+		
+		CibilScore found = this.service.findById(id).get();
+
+		 found.setFirstName(port);
+		 
+		 return found;
+	}
 
 }
