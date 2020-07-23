548c5807cdfc1c0b72b15728c15966e9b275cd2b
Srivatsan <'vatsank@gmail.com'>
2020-07-23 14:19:09 +0530

    Update

--- loan-processing-service/src/main/java/com/example/demo/LoanProcessingServiceApplication.java
+++ loan-processing-service/src/main/java/com/example/demo/LoanProcessingServiceApplication.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -22,4 +30,16 @@ public class LoanProcessingServiceApplication {
 		return new RestTemplate();
 	}
 	
+//	@Bean
+//	public CorsFilter corsfilter() {
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	    final CorsConfiguration config = new CorsConfiguration();
+//	    config.setAllowCredentials(true);
+//	    config.setAllowedOrigins(Collections.singletonList("*"));
+//	    config.setAllowedHeaders(Collections.singletonList("*"));
+//	    config.setAllowedMethods(Arrays.stream(HttpMethod.values())
+//	    		 .map(HttpMethod::name).collect(Collectors.toList()));
+//	    source.registerCorsConfiguration("/**", config);
+//	    return new CorsFilter(source);
+//	}
 }
--- loan-processing-service/src/main/java/com/example/demo/controllers/LoanProcessingController.java
+++ loan-processing-service/src/main/java/com/example/demo/controllers/LoanProcessingController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@CrossOrigin(origins = "*")
+
 public class LoanProcessingController {
 
 	
 	@Autowired
 	private RestTemplate template;
 	
-	private  String baseURL = "http://CIBIL-SCORE-SERVICE/api/v1/scores";
+	private  String baseURL = "http://LOAD-BALANCED-CIBIL/api/v1/scores";
 	
 	@GetMapping(path = "/api/v1/cibilScores",produces = "application/json")
 	public String findCibilScore() {
